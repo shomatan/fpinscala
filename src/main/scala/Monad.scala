6de776929cd3ad329dbfae2602e097ba3ea690e8
Shoma Nishitateno <shoma416@gmail.com>
2021-01-30 19:11:22 +0900

    wip: define monad

---
+++ src/main/scala/Monad.scala
@@ -0,0 +1,64 @@
+// https://github.com/fpinscala/fpinscala/blob/master/answers/src/main/scala/fpinscala/monads/Monad.scala
+
+trait Functor[F[_]] {
+
+  def map[A, B](fa: F[A])(f: A => B): F[B]
+
+  def distribute[A, B](fa: F[(A, B)]): (F[A], F[B]) =
+    (map(fa)(_._1), map(fa)(_._2))
+
+  def codistribute[A, B](e: Either[F[A], F[B]]): F[Either[A, B]] =
+    e match {
+      case Left(fa)  => map(fa)(Left(_))
+      case Right(fa) => map(fa)(Right(_))
+    }
+}
+
+object Functor {
+  val listFunctor = new Functor[List] {
+    def map[A, B](fa: List[A])(f: A => B): List[B] =
+      fa.map(f)
+  }
+}
+
+trait Monad[F[_]] extends Functor[F] {
+
+  def unit[A](a: => A): F[A]
+
+  def flatMap[A, B](ma: F[A])(a: A => F[B]): F[B]
+
+  def map[A, B](ma: F[A])(f: A => B): F[B] =
+    flatMap(ma)(a => unit(f(a)))
+
+  def map2[A, B, C](ma: F[A], mb: F[B])(f: (A, B) => C): F[C] =
+    flatMap(ma)(a => map(mb)(b => f(a, b)))
+
+  def sequence[A](lma: List[F[A]]): F[List[A]] =
+    lma.foldRight(unit(List[A]()))((ma, mla) => map2(ma, mla)(_ :: _))
+
+  def traverse[A, B](la: List[A])(f: A => F[B]): F[List[B]] =
+    la.foldRight(unit(List[B]()))((a, mlb) => map2(f(a), mlb)(_ :: _))
+
+  // Recursive version
+  def _replicateM[A](n: Int, ma: F[A]): F[List[A]] =
+    if (n <= 0) unit(List[A]()) else map2(ma, _replicateM(n - 1, ma))(_ :: _)
+
+  // Using `sequence` and the `List.fill` function of the standard library
+  def replicateM[A](n: Int, ma: F[A]): F[List[A]] =
+    sequence(List.fill(n)(ma))
+
+  def compose[A, B, C](f: A => F[B], g: B => F[C]): A => F[C] =
+    a => flatMap(f(a))(g)
+
+  def _flatMap[A, B](ma: F[A])(f: A => F[B]): F[B] =
+    compose((_: Unit) => ma, f)(())
+
+  def join[A](mma: F[F[A]]): F[A] =
+    flatMap(mma)(ma => ma)
+
+  def filterM[A](ms: List[A])(f: A => F[Boolean]): F[List[A]] =
+    ms.foldRight(unit(List[A]()))((x, y) =>
+      compose(f, (b: Boolean) => if (b) map2(unit(x), y)(_ :: _) else y)(x)
+    )
+
+}
