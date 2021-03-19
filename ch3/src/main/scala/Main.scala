86e3becee16f3281227a756a8e5fd08e560d1105
Shoma Nishitateno <shoma.nishitateno@nulab.com>
2021-03-19 19:58:39 +0900

    add: comment

--- ch3/src/main/scala/Main.scala
+++ ch3/src/main/scala/Main.scala
@@ -4,6 +4,7 @@ object Main extends App {
   case object Nil extends List[Nothing]
   case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
+  // EXERCISE 3.2
   def tail[A](list: List[A]): List[A] =
     list match {
       case Nil           => sys.error("tail of empty list")
