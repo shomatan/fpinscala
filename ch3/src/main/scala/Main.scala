282536b8450ada657d2d177fc009d4041fdcb446
Shoma Nishitateno <shoma.nishitateno@nulab.com>
2021-03-22 21:58:30 +0900

    add: practice

--- ch3/src/main/scala/Main.scala
+++ ch3/src/main/scala/Main.scala
@@ -4,10 +4,86 @@ object Main extends App {
   case object Nil extends List[Nothing]
   case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
+  object List {
+    def apply[A](as: A*): List[A] = // Variadic function syntax
+      if (as.isEmpty) Nil
+      else Cons(as.head, apply(as.tail: _*))
+  }
+
   // EXERCISE 3.2
   def tail[A](list: List[A]): List[A] =
     list match {
       case Nil           => sys.error("tail of empty list")
       case Cons(_, tail) => tail
     }
+
+  // EXERCISE 3.3
+  def setHead[A](head: A, list: List[A]): List[A] =
+    list match {
+      case Nil           => sys.error("setHead on empty list")
+      case Cons(_, tail) => Cons(head, tail)
+    }
+
+  // EXERCISE 3.4
+  def drop[A](l: List[A], n: Int): List[A] = {
+    if (n == 0) l
+    else
+      l match {
+        case Nil           => Nil
+        case Cons(_, tail) => drop(tail, n - 1)
+      }
+
+    drop(l, n)
+  }
+
+  // EXERCISE 3.5
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
+    l match {
+      case Cons(head, tail) if f(head) => dropWhile(tail, f)
+      case _                           => l
+    }
+
+  // EXERCISE 3.6
+  // println("init: " + init(Cons(1, Cons(2, Cons(3, Nil)))))
+  // init: Cons(1,Cons(2,Nil))
+  def init[A](l: List[A]): List[A] =
+    l match {
+      case Nil              => sys.error("list is empty")
+      case Cons(_, Nil)     => Nil
+      case Cons(head, tail) => Cons(head, init(tail))
+    }
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+    as match {
+      case Nil         => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  def sum2(ns: List[Int]): Int =
+    foldRight(ns, 0) { (x, y) => x + y }
+
+  def product2(ns: List[Double]): Double =
+    foldRight(ns, 1.0) { (x, y) => x * y }
+
+  // EXERCISE 3.9
+  def length[A](as: List[A]): Int =
+    foldRight(as, 0) { (_, acc) => acc + 1 }
+
+  // EXERCISE 3.10
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B =
+    as match {
+      case Nil              => z
+      case Cons(head, tail) => foldLeft(tail, f(z, head))(f)
+    }
+
+  // EXERCISE 3.11
+  def sum(ns: List[Int]): Int =
+    foldLeft(ns, 0)(_ + _)
+  def product(ns: List[Double]): Double =
+    foldLeft(ns, 1.0)(_ * _)
+
+  // EXERCISE 3.12
+  def reverse[A](list: List[A]): List[A] =
+    foldLeft(list, List[A]()) { (acc, item) => Cons(item, acc) }
+
 }
